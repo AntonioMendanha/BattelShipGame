434fd4bee97d39aaf0d7608471537ba7effb47e1
AntonioMendanha <antoniomendanha@me.com>
2022-01-12 18:26:10 -0300

    refactor: correcting utils/Verifier - and bug fixes

--- src/com/services/GameBoard.java
+++ src/com/services/GameBoard.java
@@ -1,18 +1,19 @@
 package com.services;
 
 import com.entities.BoardPosition;
+import com.entities.Player;
 import com.utils.Printer;
 
 public class GameBoard {
 
     public static final int MAX_COLUMN_ROW = 4;
+    private static final String NPC_NAME = Player.NPC_NAME;
     private static final String[] COL_HEADER_TEXT = {
-            " _ ", " A ", " B ", " C ", " D ", " E ", " F ", " G ", " H ", " I ", " J "
+        " _ ", " A ", " B ", " C ", " D ", " E ", " F ", " G ", " H ", " I ", " J "
     };
     private static final String[] ROW_HEADER_TEXT = {
-            " _ ", " 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ", " 9 ", " 10"
+        " _ ", " 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ", " 9 ", " 10"
     };
-    private static final int maxShipOnBoard = 1;
 
     /**
      * Função para criar o esqueleto do tabuleiro de células do jogo em tamanho 11 x 11
@@ -60,12 +61,12 @@ public class GameBoard {
      */
     //Imprimir o tabuleiro na tela (player ou npc)
     public static void printBoard(BoardPosition[][] board){
-       for (int i = 0; i < MAX_COLUMN_ROW ; i++){
-            for ( int j = 0; j < MAX_COLUMN_ROW ; j++) {
-                Printer.boardSpacePrinter(board[i][j]);
-            }
-            Printer.nextRow();
-        }
+       for (int i = 0; i < MAX_COLUMN_ROW; i++) {
+           for (int j = 0; j < MAX_COLUMN_ROW; j++) {
+               Printer.boardSpacePrinter(board[i][j]);
+           }
+           Printer.nextRow();
+       }
     }
 
 }
